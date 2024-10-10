ab8db40773fd047ac9204b319b2b5ebd49778f6a
otter00 <ivlevanastia2001@mail.ru>
2024-10-10 18:57:43 +0300

    first commit

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import AppRoutes from "../Routes/AppRoutes";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+};
+
+export default App;
